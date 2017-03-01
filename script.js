2dde41ceda31597abc842ba30992a4aa54b20660
TianJiang <riersy2324@gmail.com>
2017-03-01 09:55:13 -0600

    set svg

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var fields;
-
-fields = function() {
-  var currentTime, hor, min, sec;
-  currentTime = new Date();
-  sec = currentTime.getSeconds();
-  min = currentTime.getMinutes() + sec / 60;
-  hor = currentTime.getHours() + min / 60 + sec / 3600;
+//get time
+var fields = function() {
+  var currentTime = new Date();
+  var sec = currentTime.getSeconds();
+  var min = currentTime.getMinutes() + sec / 60;
+  var hor = currentTime.getHours() + min / 60 + sec / 3600;
   return data = [
     {
       "unit": "seconds",
@@ -20,8 +18,7 @@ fields = function() {
   ];
 };
 
-var w = 400;
-var h = 200;
+//set scale
 var offSetX = 150;
 var offSetY = 100;
 var pi = Math.PI;
@@ -34,3 +31,23 @@ var scaleMins = d3.scale.linear()
 var scaleHors = d3.scale.linear()
                         .domain([0, 11 + 59/60])
                         .range([0, 2 * pi]);
+
+
+//set svg
+var width = 400;
+var height = 200;
+var svg = d3.selectAll(".chart")
+            .append("svg:svg")
+            .attr("width", width)
+            .attr("height", height);
+var clockSet = svg.append("svg:g") 
+                  .attr("transform", "translate(" + offSetX + "," + offSetY + ")"); 
+clockSet.append("svg:circle")
+        .attr("r", 80)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
+clockSet.append("svg:circle")
+        .attr("r", 4)
+        .attr("fill", "black")
+        .attr("class", "clock innercircle");
